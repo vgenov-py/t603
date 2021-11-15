5f89a566ed9d34396eec85babd44762c588853ac
CICE <genovese.work@protonmail.com>
2021-11-15 21:22:10 +0100

    testing

--- bookshop/bookshop.py
+++ bookshop/bookshop.py
@@ -59,6 +59,10 @@ DB = [{ # Esto ha sido blah nano
     "genre": "Narrativa extranjera"
 },
 ]
+
+def export_csv(file_name):
+    pass
+
 genres = ["Narrativa extranjera", "Divulgación científica", "Narrativa policíaca", "Ciencia ficción", "Autoayuda"]
 
 user = "0"
@@ -98,37 +102,6 @@ def update_book(book):
         if user:
             book[k] = user
 
-# while user != "q":
-#     menu()
-#     user = input(": ")
-#     if user == "1":
-#         user = input("ISBN: ")
-#         book_found = look_for_id(user)
-#         if book_found:
-#             pretty_book(book_found)
-#         else:
-#             print(f"No encontramos el libro con el ISBN: {user}")
-#         input()
-        
-#     elif user == "4":
-#         for i, genre in enumerate(genres):
-#             print(f"{i + 1}. {genre}")
 
-#         user = int(input("Género: ")) -1
-#         user = genres[user]
-#         books = get_by_term("genre", user)
-#         for i, book in enumerate(books):
-#             pretty_book(book)
-#             print("-"*30)
-#             if i % 2 == 0:
-#                 input("Siguiente: ")
-#         input()
-
-#     elif user == "5":
-#         user = input("Buscar libro a modificar por ID: ")
-#         book_to_update = look_for_id(user)
-#         if book_to_update:
-#             update_book(book_to_update)
-#         print(DB)
 
 
