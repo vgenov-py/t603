f27bf93a6546226fd482f487985209da9bea9815
CICE <genovese.work@protonmail.com>
2021-11-17 21:52:38 +0100

    Municipios de Madrid

---
+++ municipios/main.py
@@ -0,0 +1,75 @@
+import requests as req
+import csv
+
+# url = "https://datos.comunidad.madrid/catalogo/dataset/032474a0-bf11-4465-bb92-392052962866/resource/ee750429-1e05-411a-b026-a57ea452a34a/download/municipio_comunidad_madrid.csv"
+# res = req.get(url).content
+# res = res.decode("utf8", errors="replace") # NOS DEVUELVE UNA STRING
+
+# with open("data.csv", mode="w", encoding="utf8") as file:  # Escritura del fichero descargado
+#     file.write(res)
+
+def get_by_ine(dataset, ine_code):
+    result = None
+    for mun in dataset[1:]:
+        if mun[2] == ine_code:
+            result = mun
+    return result
+
+
+# 3. Obtener superficie total:
+
+def get_sup_total(dataset):
+    counter = 0
+    for i, mun in enumerate( dataset[1:]):
+        counter = counter + float(mun[-2])
+    return counter
+        
+def get_sup_total_2(dataset):
+    result = []
+    for mun in dataset[1:]:
+        result.append(float(mun[-2]))
+    return sum(result)
+
+def get_sup_total_3(dataset):
+    result = [float(mun[-2]) for mun in dataset[1:]]
+    return sum(result)
+    # return sum([float(mun[-2]) for mun in dataset])
+
+def get_biggest_mun(dataset):
+    big_mun = None
+    sup_to_beat = 0
+    for mun in dataset[1:]:
+        if float(mun[-2]) > sup_to_beat:
+            big_mun = mun
+            sup_to_beat = float(mun[-2])
+    return big_mun
+
+def get_biggest_mun_2(dataset):
+    max_sup = max([float(mun[-2]) for mun in dataset[1:]])
+    for mun in dataset[1:]:
+        if float(mun[-2]) == max_sup:
+            return mun
+        
+with open("./data.csv", mode="r", encoding="utf8") as file:
+    data = list(csv.reader(file,delimiter=";"))
+# sup total:
+# print(get_sup_total_3(data))
+
+# biggest mun:
+# print(get_biggest_mun_2(data))
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ municipios/venv.md
@@ -0,0 +1,21 @@
+#### Comandos venv
+
+* **python3** -m venv <venv_name>
+* (UNIX) source venv/bin/activate 
+* (W10) source venv/Scripts/activate
+* deactivate
+
+### Instalar una libería/módulo
+* (entorno virtual encendido)
+* pip install <librería>
+
+### Saber qué tenemos instalado / consultar dependencias
+
+* pip freeze
+
+* Crear un txt con las dependencias:
+```bash
+pip freeze > <text_file.txt>
+```
+
+
