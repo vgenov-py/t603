079e290f8590ad82627177dfb68b33455b36edad
CICE <genovese.work@protonmail.com>
2022-01-17 21:54:08 +0100

    SQLITE update

--- server/test_db.py
+++ server/test_db.py
@@ -4,6 +4,12 @@ import sqlite3
 con = sqlite3.connect("./bookshop.db")
 cur = sqlite3.Cursor(con)
 
+# cur.execute('''CREATE TABLE dogs (id TEXT PRIMARY KEY, name TEXT, breed TEXT, age INTEGER);''')
+# cur.execute(f'''INSERT INTO dogs VALUES("D_0001", "Kuga", "Chucho", 7);''')
+# a = cur.execute('''SELECT * FROM dogs;''')
+# print(a)
+# con.commit()
+
 cur.execute('''
 CREATE TABLE books (
     id TEXT PRIMARY KEY,
