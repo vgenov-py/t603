58607f27647c387a6f2935beda198f0cb0a34f46
CICE <genovese.work@protonmail.com>
2022-01-18 21:56:21 +0100

    18012021

--- server/main.py
+++ server/main.py
@@ -1,12 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from funcs import *
-from flask_cors import CORS
+import sqlite3 
 
 app = Flask(__name__)
-CORS(app)
 
+DB="./bookshop.db"
 
-DB="bookshop.json"
+def con():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DB)
+    return db
 
 @app.route("/")
 def home():
@@ -31,6 +35,38 @@ def test():
 @app.route("/all")
 def all():
     return get_all(DB)
+
+    
+
+
+
+
+
+
+
+
+@app.route("/db_all")
+def get_db_all():
+    cur = con().cursor()
+    result = {"data": []}
+    fields = tuple(field[1] for field in cur.execute("PRAGMA table_info(books);"))
+    query = "SELECT * FROM books"
+    if request.args:
+        query += f"ORDER BY {request.args['sort']};"
+    else:
+        query += ";"
+    query = f"SELECT * FROM books ORDER BY {request.args['sort']}" if request.args else "SELECT * FROM books"
+    for book in cur.execute(query):
+        # dict_book = {}
+        # for i, field in enumerate(fields):
+        #     dict_book[field] = book[i]
+        # result["data"].append(dict_book)
+        result["data"].append(dict(zip(fields, book)))
+    
+
+    
+    return result
+
 
 @app.route("/book/<book_id>", methods=["GET", "DELETE", "PUT"])
 def book_by_id(book_id):
--- server/test_db.py
+++ server/test_db.py
@@ -1,5 +1,7 @@
 import sqlite3
+import json
 
+from funcs import get_by_id
 
 con = sqlite3.connect("./bookshop.db")
 cur = sqlite3.Cursor(con)
@@ -10,11 +12,43 @@ cur = sqlite3.Cursor(con)
 # print(a)
 # con.commit()
 
-cur.execute('''
-CREATE TABLE books (
-    id TEXT PRIMARY KEY,
-    title TEXT,
-    author TEXT,
-    genre TEXT,
-    stock INTEGER
-    );''')
+# cur.execute('''
+# CREATE TABLE books (
+#     id TEXT PRIMARY KEY,
+#     title TEXT,
+#     author TEXT,
+#     genre TEXT,
+#     stock INTEGER
+#     );''')
+# INSERT INTO table VALUES ()
+def get_all(json_file):
+    with open(json_file, encoding="utf8") as file:
+        return json.load(file)
+
+books = get_all("./bookshop.json")["data"]
+b_1 = books[1]
+print(tuple(b_1.values()))
+
+# cur.execute(f"DELETE FROM books where stock >= 0")
+
+# BULK INSERTION
+
+# for book in books:
+#     cur.execute(f"INSERT INTO books VALUES {tuple(book.values())}")
+
+# con.commit()
+
+# query = "INSERT INTO books VALUES"
+
+# for i, book in enumerate(books):
+#     if i < len(books) -1:
+#         query += f"{tuple(book.values())},"
+#     else:
+#         query += f"{tuple(book.values())};"
+
+
+# print(query)
+
+# cur.execute(query)
+
+# con.commit()
