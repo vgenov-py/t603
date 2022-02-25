10d8d8e4c95a3b4b51ab9e228575fcdf8c2ef0bc
CICE <genovese.work@protonmail.com>
2022-02-25 21:13:29 +0100

    FR

--- pcep/main.py
+++ pcep/main.py
@@ -1,3 +1,4 @@
+from random import shuffle
 from click import option
 from flask import Flask, make_response, session, request, render_template, Response
 from uuid import uuid4
@@ -10,15 +11,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_URI}"
 db.init_app(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def query():
-    question = Question.query.first()
-    print(Option.query.filter_by(question_id = question.id).all() == question.options)
-
-
-    # db.session.commit()
-
-    return render_template("index.html")
+    result = 0
+    questions = Question.query.all()
+    return render_template("index.html", questions=questions)
 
 if __name__ == "__main__":
     app.run(debug=True)
