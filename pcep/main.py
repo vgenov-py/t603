06bc7038889bacf3298fcf01a6489b553cb2aa8b
CICE <genovese.work@protonmail.com>
2022-02-25 19:31:55 +0100

    LAST

--- pcep/main.py
+++ pcep/main.py
@@ -1,16 +1,23 @@
-from flask import Flask, session, request, render_template
+from click import option
+from flask import Flask, make_response, session, request, render_template, Response
 from uuid import uuid4
 from hashlib import sha256
-from models import db, User
+from models import db, User, Question, Option
 
 app = Flask(__name__)
 DB_URI = "multitest.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_URI}"
 db.init_app(app)
 
+
 @app.route("/")
 def query():
-    print(User.query.all())
+    question = Question.query.first()
+    print(Option.query.filter_by(question_id = question.id).all() == question.options)
+
+
+    # db.session.commit()
+
     return render_template("index.html")
 
 if __name__ == "__main__":
--- pcep/models.py
+++ pcep/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 class User(db.Model):
@@ -10,3 +11,25 @@ class User(db.Model):
 
     def __repr__(self):
         return f"{self.id},{self.email}"
+
+class Question(db.Model):
+    id = db.Column(db.String(32), primary_key=True)
+    q = db.Column(db.Text, unique=True, nullable=False)
+    a = db.Column(db.String(32), nullable=True)
+
+    options = db.relationship("Option", backref=db.backref("question", lazy=True))
+
+    # @property Igual a option de arriba
+    # def options(self):
+    #     return Option.query.filter_by(question_id=self.id).all()
+
+    def __repr__(self):
+       return f"{self.q}"
+
+class Option(db.Model):
+    id = db.Column(db.String(32), primary_key=True)
+    o = db.Column(db.Text, nullable=False)
+    question_id = db.Column(db.String(32), ForeignKey("question.id"))
+
+    def __repr__(self):
+       return f"{self.o}"
